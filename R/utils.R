9bed12cfd7e7b9746772cdb2c9d1f1a9c19126b1
Jeanette Clark <jclark@nceas.ucsb.edu>
2018-09-20 09:27:11 -0700

    add function

--- R/utils.R
+++ R/utils.R
@@ -72,4 +72,36 @@ annual_wave_power <- function(path, start_year, end_year, deg_bins){
 
 
 
+#
+# Splining a polygon.
+#
+#   The rows of 'xy' give coordinates of the boundary vertices, in order.
+#   'vertices' is the number of spline vertices to create.
+#              (Not all are used: some are clipped from the ends.)
+#   'k' is the number of points to wrap around the ends to obtain
+#       a smooth periodic spline.
+#
+#   Returns an array of points. 
+# 
+spline.poly <- function(xy, vertices, k=3, ...) {
+    # Assert: xy is an n by 2 matrix with n >= k.
+    
+    # Wrap k vertices around each end.
+    n <- dim(xy)[1]
+    if (k >= 1) {
+        data <- rbind(xy[(n-k+1):n,], xy, xy[1:k, ])
+    } else {
+        data <- xy
+    }
+    
+    # Spline the x and y coordinates.
+    data.spline <- spline(1:(n+2*k), data[,1], n=vertices, ...)
+    x <- data.spline$x
+    x1 <- data.spline$y
+    x2 <- spline(1:(n+2*k), data[,2], n=vertices, ...)$y
+    
+    # Retain only the middle part.
+    cbind(x1, x2)[k < x & x <= n+k, ]
+}
+
 
